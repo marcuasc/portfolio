2e70a72987500f84386a9d6f1cf739d71f284076
marcuasc <marcus6623@hotmail.com>
2021-03-15 21:34:09 +0100

    Fixed component for frontpage

--- portfolio/src/Components/FrontPage/index.jsx
+++ portfolio/src/Components/FrontPage/index.jsx
@@ -8,8 +8,10 @@ import Menu from "../Menu/index"
 const FrontPage = () => {
   return (
       <>
-    <div className="flex-row bg-middleGrey min-h-screen w-screen block">
+    <div className="flex-row bg-blueGray min-h-screen w-screen block">
    <Menu></Menu>
+
+   <div className="w-3/5 h-72 bg-gray-300 m-auto mt-52 rounded-md" ></div>
     </div>
     </>
   );
--- portfolio/src/Components/Projects/index.jsx
+++ portfolio/src/Components/Projects/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import webdev from "/Users/marcus/Desktop/nettside/portfolio/portfolio/src/Data/webdev.png";
 
 
 
@@ -8,10 +9,33 @@ const Projects = () => {
       <>
     <div className="flex-row bg-blueGray min-h-screen w-screen transform rotate-rq-8 block p-7">
 
-        <div className=" flex-col w-1/2 h-80 rounded-md bg-blue-300 m-auto mb-5 mt-10 " >
-        <div className=" flex w-1/2 h-80 rounded-l-md bg-blue-600" ></div>
+        <div className=" flex flex-row w-1/2 h-80 rounded-md bg-blue-300 m-auto mb-5 mt-60 " >
+        <div className=" flex w-1/2 h-80 rounded-l-md bg-blue-600 " ></div>
+        <div className="flex w-1/2 h-80 rounded-r-md bg-blue-600" >
+        <img src= {webdev} alt="webdev" className="flex w-full h-80 rounded-r-md " />
         </div>
-      
+        </div>
+
+        <div className=" flex flex-row w-1/2 h-80 rounded-md bg-blue-300 m-auto mb-5 mt-32 " >
+        <div className=" flex w-1/2 h-80 rounded-l-md bg-blue-600 " ></div>
+        <div className="flex w-1/2 h-80 rounded-r-md bg-blue-600" >
+        <img src= {webdev} alt="webdev" className="flex w-full h-80 rounded-r-md " />
+        </div>
+        </div>
+
+        <div className=" flex flex-row w-1/2 h-80 rounded-md bg-blue-300 m-auto mb-5 mt-32 " >
+        <div className=" flex w-1/2 h-80 rounded-l-md bg-blue-600 " ></div>
+        <div className="flex w-1/2 h-80 rounded-r-md bg-blue-600" >
+        <img src= {webdev} alt="webdev" className="flex w-full h-80 rounded-r-md " />
+        </div>
+        </div>
+
+        <div className=" flex flex-row w-1/2 h-80 rounded-md bg-blue-300 m-auto mb-5 mt-32 " >
+        <div className=" flex w-1/2 h-80 rounded-l-md bg-blue-600 " ></div>
+        <div className="flex w-1/2 h-80 rounded-r-md bg-blue-600" >
+        <img src= {webdev} alt="webdev" className="flex w-full h-80 rounded-r-md " />
+        </div>
+        </div>
     </div>
     </>
   );
