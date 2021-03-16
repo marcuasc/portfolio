9e9727c981b347540ede37b101c66f98fa10a0e6
marcuasc <marcus6623@hotmail.com>
2021-03-16 01:15:09 +0100

    Removed target_blank, fixed deployment

--- portfolio/src/Components/Footer/index.jsx
+++ portfolio/src/Components/Footer/index.jsx
@@ -11,11 +11,11 @@ const Footer = () => {
     <div className = "flex flex-col max-w-screen-2xl w-screen bg-red-300 h-96 z-30">
       <div className = " flex  h-1/2 w-1/2 m-auto justify-between " > 
 
-      <a href="https://github.com/marcuasc" target="_blank">
+      <a href="https://github.com/marcuasc">
       <FaGithubSquare size={70} />
       </a>
 
-      <a href="https://www.linkedin.com/in/marcus-schr%C3%B8der/" target="_blank">
+      <a href="https://www.linkedin.com/in/marcus-schr%C3%B8der/" >
         <FaLinkedin size={70}/>
       </a>
 
