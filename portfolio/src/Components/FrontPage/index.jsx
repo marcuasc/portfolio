e2f38fa00c307ce49cc09aaedc6a40e6859e1191
marcuasc <marcus6623@hotmail.com>
2021-03-16 01:05:29 +0100

    Fixed project header

--- portfolio/src/Components/FrontPage/index.jsx
+++ portfolio/src/Components/FrontPage/index.jsx
@@ -12,7 +12,7 @@ const FrontPage = () => {
     <div className="flex-row bg-blueGray min-h-screen w-screen block">
    <Menu></Menu>
 
-   <div className="w-3/5 h-72 bg-gray-300 m-auto mt-52 rounded-xl p-2 font-sans bg-opacity-30 text-4xl text-red-300" >
+   <div className="w-3/5 h-40 bg-gray-300 m-auto mt-36 rounded-xl p-2 font-sans bg-opacity-30 text-4xl text-red-300" >
 
    <Typewriter
   onInit={(typewriter) => {
--- portfolio/src/Components/Projects/index.jsx
+++ portfolio/src/Components/Projects/index.jsx
@@ -9,11 +9,11 @@ const Projects = () => {
       <>
     <div className="flex-row bg-blueGray min-h-screen w-screen transform rotate-rq-8 block p-7">
 
-      <div className="flex flex-col w-1/3 h-8 m-auto justify-center justify bg-red-300 p-8 text-3xl" >
-        <h1>Prosjekter</h1>
+      <div className="flex flex-col w-1/5 h-8 m-auto justify-center bg-red-300 p-8 text-3xl" >
+        <h1 className="m-auto" >Prosjekter</h1>
         </div>
 
-        <div className=" flex flex-row w-3/5 h-96 rounded-lg bg-gradient-to-r from-green-400 to-blue-500 m-auto mb-5 mt-60 shadow-2xl" >
+        <div className=" flex flex-row w-3/5 h-96 rounded-lg bg-gradient-to-r from-green-400 to-blue-500 m-auto mb-5 mt-32 shadow-2xl" >
         <div className=" flex flex-col w-1/2 h-96 rounded-l-lg border-indigo-100 p-8" >
         <div class="uppercase tracking-wide text-2xl text-black font-semibold">Case study</div>
         <p className =" text-white font-semibold mt-5 " > Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers. </p>
